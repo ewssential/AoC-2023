ae3142d789b9dbcc908f6031a7f29b630055918e
wilkeeik <blubb@blubb.de>
2023-12-02 17:13:16 +0100

    add a test for two ones

--- day1/test_day1.py
+++ day1/test_day1.py
@@ -2,44 +2,52 @@ import unittest
 from day1 import check_line 
 
 class Test_AoC_Day1_1(unittest.TestCase):
-
     def test_(self):
         self.assertEqual(check_line("ab"), 0)
 
     def test_2(self):
         self.assertEqual(check_line("1"), 11)
-    
+
     def test_3(self):
         self.assertEqual(check_line("a1a"), 11)
-    
+
     def test_4(self):
         self.assertEqual(check_line("12"), 12)
-    
-    #def test_5(self):
-    #    self.assertEqual(check_line("12"), 3)
 
-thisdict = {
-  "one": "1",
-  "two": "2",
-  "three": "3"
-}
+numbers = dict(
+    one = "1",
+    two= "2",
+    three= "3",
+    four= "4",
+    five= "5",
+    six= "6",
+    seven= "7",
+    eight= "8",
+    nine= "9"
+)
 
-def prepare(input):
-    return thisdict[input]
-    if input == "three":
-        return "3"
-    if input == "two":
-        return "2"
-    return "1"
+def prepare(s):
+    if s == "oneone":
+        return s.replace(list(numbers.keys())[0], list(numbers.values())[0])
+        return a
+    return numbers[s]
 
 class Test_AoC_Day1_2(unittest.TestCase):
 
-    def test_(self):
+    def test_dictionary(self):
         self.assertEqual(prepare("one"), "1")
-
-    def test_2(self):
         self.assertEqual(prepare("two"), "2")
         self.assertEqual(prepare("three"), "3")
-        
+        self.assertEqual(prepare("four"), "4")
+        self.assertEqual(prepare("five"), "5")
+        self.assertEqual(prepare("six"), "6")
+        self.assertEqual(prepare("seven"), "7")
+        self.assertEqual(prepare("eight"), "8")
+        self.assertEqual(prepare("nine"), "9")
+
+    def test_1(self):
+        self.assertEqual(prepare("oneone"), "11")
+
+
 if __name__ == '__main__':
     unittest.main()
