52125c273c0b9cdd42d8bb822bcafe59ccb7bbea
wilkeeik <blubb@blubb.de>
2023-12-02 14:15:49 +0100

    use function

--- day1/day1.py
+++ day1/day1.py
@@ -16,3 +16,13 @@ def check_line(str):
     return iterate(str) + iterate(str[::-1])
 
 
+def day11():
+    f = open("input.txt", "r")
+    count = 0
+    for line in f:
+        count = count + check_line(line)
+    print(count)
+    
+    
+if __name__ == '__main__':
+    day11()
