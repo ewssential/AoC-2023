66c456ec7a55ce5a1fb25c4513af7e77ffbae1f6
wilkeeik <blubb@blubb.de>
2023-12-02 14:19:08 +0100

    fixed wrong understanding

--- day1/test_day1.py
+++ day1/test_day1.py
@@ -7,13 +7,15 @@ class Test_AoC_Day1(unittest.TestCase):
         self.assertEqual(check_line("ab"), 0)
 
     def test_2(self):
-        self.assertEqual(check_line("1"), 2)
+        self.assertEqual(check_line("1"), 11)
     
     def test_3(self):
-        self.assertEqual(check_line("a1a"), 2)
+        self.assertEqual(check_line("a1a"), 11)
     
     def test_4(self):
-        self.assertEqual(check_line("12"), 3)
+        self.assertEqual(check_line("12"), 12)
     
+    #def test_5(self):
+    #    self.assertEqual(check_line("12"), 3)
 if __name__ == '__main__':
     unittest.main()
